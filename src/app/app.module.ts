5f139860f89bb90b625ce52d183c99f80c68ad48
sawyerbutton <lil103@pitt.edu>
2018-05-31 19:01:25 -0400

    added a basic http client feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -26,6 +27,7 @@ import {TodoListService} from './todo-list.service';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
     // Material Modules
     MatToolbarModule,
     MatIconModule,
--- src/app/todo-list.service.ts
+++ src/app/todo-list.service.ts
@@ -1,27 +1,42 @@
 import { Injectable } from '@angular/core';
 import { TodoItem} from './shared/todo-item';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
 
-  constructor() { }
-  todoItems: TodoItem[] = [{
-    id: 1,
-    value: 'Todo Item No.1',
-    done: false
-  }, {
-    id: 2,
-    value: 'Todo Item No.2',
-    done: false
-  }, {
-    id: 3,
-    value: 'Todo Item No.3',
-    done: false
-  }];
+  constructor(
+    // Module for Http function
+    private http: HttpClient
+  ) {
+    this.loadTodoList();
+  }
+  // static todoItems
+  // todoItems: TodoItem[] = [{
+  //   id: 1,
+  //   value: 'Todo Item No.1',
+  //   done: false
+  // }, {
+  //   id: 2,
+  //   value: 'Todo Item No.2',
+  //   done: false
+  // }, {
+  //   id: 3,
+  //   value: 'Todo Item No.3',
+  //   done: false
+  // }];
+  public todoItems: TodoItem[];
   getTodoList() {
     return this.todoItems;
+  }
+  loadTodoList() {
+    this.http.get<TodoItem[]>('/assets/todo-list.json')
+      .subscribe(res => this.todoItems = res);
+    // .toPromise() this is convert observable to promise, need toPromise function from rxjs
+    //       .then(response => {
+    //         this.todoItems = response.json();
   }
   addTodoList(text) {
     this.todoItems.push(
